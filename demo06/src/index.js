e985ed7a34e2669e16173be1bfc854f48d65708d
happydxh <1109747853@qq.com>
2019-08-27 09:54:45 +0800

    demo06

---
+++ demo06/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>app</div>
+    )
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'))
